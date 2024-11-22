2bba3eb6e5a133c488c0247cf46c5ef390e3d966
PantyDev <pantypon@gmail.com>
2024-11-22 09:41:21 +0200

    feat: done with todo list

--- script.js
+++ script.js
@@ -24,18 +24,27 @@ const renderItems = () => {
     
         const element = document.createElement("div");
         element.classList.add("item");
-    
+
+        const value = `${data.text}`;
+
         const textElement = document.createElement("span");
         textElement.classList.add("text");
-        textElement.innerText = `${data.id} ${data.text}`;
+        textElement.innerText = value;
     
+        const inputEditElement = inputSubmit.cloneNode();
+        inputEditElement.value = value;
+        inputEditElement.style.display = "none";
+
         element.append(textElement);
+        element.append(inputEditElement);
     
         const typeElement = document.createElement("span");
         typeElement.classList.add("type");
         typeElement.innerText = selectValues[data.type];
 
-
+        const selectEditElement = selectSubmit.cloneNode(true);
+        selectEditElement.value = data.type;
+        selectEditElement.style.display = "none";
 
         const editButtonElement = document.createElement("button");
         editButtonElement.innerText = "Редагувати";
@@ -44,7 +53,9 @@ const renderItems = () => {
         deleteButtonElement.innerText = "Видалити";
 
         const controlsWrapperElement = document.createElement("div");
+        controlsWrapperElement.classList.add("controls-wrapper")
         controlsWrapperElement.append(typeElement);
+        controlsWrapperElement.append(selectEditElement);
         controlsWrapperElement.append(editButtonElement);
         controlsWrapperElement.append(deleteButtonElement);
 
@@ -59,13 +70,61 @@ const renderItems = () => {
             
             renderItems();
         });
+
+
+
+        let isEdit = false;
+        editButtonElement.addEventListener("click", () => {
+            isEdit = !isEdit;
+            
+            if(isEdit) {
+                textElement.style.display = "none";
+                typeElement.style.display = "none";
+
+                inputEditElement.style.display = "block";
+                selectEditElement.style.display = "block";
+               
+
+                editButtonElement.innerText = "Зберегти";
+
+                
+                
+                return;
+            }
+            textElement.style.display = "block";
+            typeElement.style.display = "block";
+            
+            inputEditElement.style.display = "none";
+            selectEditElement.style.display = "none";
+
+            const index = dataArray.findIndex(x => x.id === data.id);
+            console.log(data.id)
+            dataArray[index] = {
+                id: data.id,
+                text: inputEditElement.value,
+                type: selectEditElement.value
+            };
+            localStorage.setItem("list", JSON.stringify(dataArray)); 
+            renderItems();
+
+            editButtonElement.innerText = "Редагувати";
+        })
     }
 };
 
-renderItems();
 initSelect();
+renderItems();
+
+
+buttonSubmit.disabled = !inputSubmit.value;
+inputSubmit.addEventListener("input", () => {
+    buttonSubmit.disabled = !inputSubmit.value;
+});
 
 buttonSubmit.addEventListener("click", () => {
+    const inputValue = inputSubmit.value;
+    if(!inputValue) return;
+
     dataArray.push({
         id: dataArray[dataArray.length - 1]?.id + 1 || 0,
         text: inputSubmit.value,
